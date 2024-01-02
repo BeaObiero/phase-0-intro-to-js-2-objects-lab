8e7b95288eca9f3125165ab153e7a6fbf9ac20e5
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-01-02 18:49:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
+
+
+
+//update employee with key and value and return new value without ammending the original object
+function updateEmployeeWithKeyAndValue(key,value) {
+    return {...employee,[key]: value};
+}
+
+//destructively update employee with key and value, should mutate original employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(key,value){
+    employee[key] = value;
+}
+
+//delete from employee by key, deleting key, return new object without mutating original object
+function deleteFromEmployeeByKey(key){
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+}
+
+//destructivelyDestroyFromEmployeeByKey
+function destructivelyDeleteFromEmployeeByKey(key){
+    delete employee[key];
+    return employee
+}
+
+
+
